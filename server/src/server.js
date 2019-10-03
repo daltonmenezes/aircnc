422fe7b0732f48baeb2e7f7bc518e8e8162e65d6
Dalton Menezes <daltonmenezes@outlook.com>
2019-10-02 21:27:31 -0300

    Add cors

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose')
 const express = require('express')
 const routes = require('./routes')
 const app = express()
+const cors = require('cors')
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-nmejl.mongodb.net/${process.env.MONGO_COLLECTION}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
